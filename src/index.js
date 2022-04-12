8e215a2612e123e4aebbe2dd34bbdd64e9226ec6
jamel <jay.redd19@gmail.com>
2022-04-12 18:04:43 -0400

    3 done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// Mouseover
+let navLinks = document.querySelectorAll('.nav').forEach(navLinks => navLinks.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+}));
+
+// Double Click ang Toggle
+let biggerImg = document.querySelector('img')
+biggerImg.addEventListener('dblclick', function(event) {
+    biggerImg.classList.toggle('large');
+});
+
+// 
