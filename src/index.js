1ce53dff2e5c2c3d2d46250e66299d5602507a21
jamel <jay.redd19@gmail.com>
2022-04-12 19:54:39 -0400

    almost done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ let navLinks = document.querySelectorAll('.nav').forEach(navLinks => navLinks.ad
     event.target.style.color = 'red';
 }));
 
+let titles = document.querySelectorAll('h2').forEach(titles => titles.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'blue';
+}));
+
+//Mouseleave
+navLinks = document.querySelectorAll('.nav').forEach(navLinks => navLinks.addEventListener('mouseleave', function(event) {
+    event.target.style.color = 'black';
+}));
+
+navLinks = document.querySelectorAll('h4').forEach(navLinks => navLinks.addEventListener('mouseleave', function(event) {
+    event.target.style.color = 'black';
+}));
+
 // Double Click ang Toggle
 let biggerImg = document.querySelector('img')
 biggerImg.addEventListener('dblclick', function(event) {
@@ -14,3 +27,8 @@ biggerImg.addEventListener('dblclick', function(event) {
 });
 
 // 
+// MouseDown
+
+const clickDown = document.querySelectorAll('h4').forEach(clickDown => clickDown.addEventListener('mousedown', function(event) {
+    alert('Grrr!');
+}));
